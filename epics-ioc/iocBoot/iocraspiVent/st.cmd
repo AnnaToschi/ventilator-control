6477e2da7b5379b8ddb4f3a20ab424650ac79f24
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2020-03-19 16:11:27 +0000

    Initial import EPICS IOC

---
+++ epics-ioc/iocBoot/iocraspiVent/st.cmd
@@ -0,0 +1,31 @@
+#!../../bin/linux-arm/raspiVent
+
+#- You may have to change raspiVent to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/raspiVent.dbd"
+raspiVent_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadTemplate "db/user.substitutions"
+dbLoadRecords "db/raspiVentVersion.db", "user=pi"
+dbLoadRecords "db/rpiControl.db", "P=Raspi:,R=central:"
+dbLoadRecords "db/rpiSensors-simul.db", "P=Raspi:,R=central:"
+#dbLoadRecords "db/dbSubExample.db", "user=pi"
+
+#- Set this to see messages from mySub
+#var mySubDebug 1
+
+#- Run this to trace the stages of iocInit
+#traceIocInit
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncExample, "user=pi"
