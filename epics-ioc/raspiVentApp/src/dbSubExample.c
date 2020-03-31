b3691d286e4dbf4780d5e3cfada391878d5212da
Bernardo <bernardo.carvalho@tecnico.ulisboa.pt>
2020-04-01 00:52:24 +0100

    adde sdSubExample

--- epics-ioc/raspiVentApp/src/dbSubExample.c
+++ epics-ioc/raspiVentApp/src/dbSubExample.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
 
+#include <aSubRecord.h>
 #include <dbDefs.h>
+#include <epicsExport.h>
 #include <registryFunction.h>
 #include <subRecord.h>
-#include <aSubRecord.h>
-#include <epicsExport.h>
+
+#include <math.h>
+
+const float flow[]= {
+#include "flow.txt"
+};
+
 
 int mySubDebug;
 
-static long mySubInit(subRecord *precord)
-{
-    if (mySubDebug)
-        printf("Record %s called mySubInit(%p)\n",
-               precord->name, (void*) precord);
-    return 0;
+static long mySubInit(subRecord *precord) {
+  if (mySubDebug)
+    printf("Record %s called mySubInit(%p)\n", precord->name, (void *)precord);
+  return 0;
 }
 
-static long mySubProcess(subRecord *precord)
-{
-    if (mySubDebug)
-        printf("Record %s called mySubProcess(%p)\n",
-               precord->name, (void*) precord);
-    return 0;
+static long mySubProcess(subRecord *precord) {
+  int idx;
+  if (mySubDebug)
+    printf("Record %s called mySubProcess(%p)\n", precord->name,
+           (void *)precord);
+  idx = floor(20 * precord->a);
+  idx = (idx < 0)? 0: idx; // Check LUT limits 
+  idx = (idx > 199)? 199: idx;
+  precord->val = flow[idx];
+  // precord->val++;
+  return 0;
 }
 
-static long myAsubInit(aSubRecord *precord)
-{
-    if (mySubDebug)
-        printf("Record %s called myAsubInit(%p)\n",
-               precord->name, (void*) precord);
-    return 0;
+static long myAsubInit(aSubRecord *precord) {
+  if (mySubDebug)
+    printf("Record %s called myAsubInit(%p)\n", precord->name, (void *)precord);
+  return 0;
 }
 
-static long myAsubProcess(aSubRecord *precord)
-{
-    if (mySubDebug)
-        printf("Record %s called myAsubProcess(%p)\n",
-               precord->name, (void*) precord);
-    return 0;
+static long myAsubProcess(aSubRecord *precord) {
+  if (mySubDebug)
+    printf("Record %s called myAsubProcess(%p)\n", precord->name,
+           (void *)precord);
+  return 0;
 }
 
 /* Register these symbols for use by IOC code: */
